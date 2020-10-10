6067b26724e1c2ef6d169712ef0087739bd14348
Andrew Siegel <siegelal7@gmail.com>
2020-10-10 14:02:43 -0400

    sequelize still

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,4 +7,19 @@ module.exports = function (app) {
       res.render("index", { response });
     });
   });
+
+  app.post("/api/people", function (req, res) {
+    // connection.query(
+    //   "insert into people (person_name, age, is_threat, city_name) values (?, ?, ?, ?)",
+    //   [req.body.name, req.body.age, req.body.threat, req.body.city],
+    //   function (err, data) {
+    //     if (err) throw err;
+    //     res.status(200).end();
+    //   }
+    // );
+    var body = req.body;
+    db.create(body).then(function (results) {
+      res.json(results);
+    });
+  });
 };
